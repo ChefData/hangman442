8080df943783c4d0ba29f8a5611811e88123128d
ChefData <150172835+ChefData@users.noreply.github.com>
2023-11-14 10:15:55 +0000

    Update milestone_3.py
    Task 2: Check whether the guess is in the word

--- milestone_3.py
+++ milestone_3.py
@@ -1,6 +1,17 @@
+import random
+
+word_list = ["Mango", "Pineapple", "Mangosteen", "Lychee", "Fig"]
+random_word = random.choice(word_list)
+
 while True:
     letter_guess = input("Enter a single letter: ")
     if len(letter_guess) == 1 and letter_guess.isalpha():
         break
     else:
         print("Invalid letter. Please, enter a single alphabetical character.")
+
+
+if letter_guess in random_word:
+    print(f"Good guess! {letter_guess} is in the word.")
+else:
+    print(f"Sorry, {letter_guess} is not in the word. Try again.")
