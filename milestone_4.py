c1f74dce8f39f753d88f6416ac75beda7860cd52
ChefData <150172835+ChefData@users.noreply.github.com>
2023-11-14 15:54:25 +0000

    Update milestone_4.py
    Task 4: Define what happens if the letter is NOT in the word

--- milestone_4.py
+++ milestone_4.py
@@ -25,7 +25,9 @@ class Hangman:
                     self.word_guessed[char] = lower_case_guess
             self.num_letters -= 1
         else:
+            self.num_lives -= 1
             print(f"Sorry, {lower_case_guess} is not in the word. Try again.")
+            print(f"You have {self.num_lives} lives left.")
 
     def ask_for_input(self):
         while True:
