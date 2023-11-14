f037865922b017d433e0034ca1da1bbb8017871e
ChefData <150172835+ChefData@users.noreply.github.com>
2023-11-14 10:24:16 +0000

    Update milestone_3.py
    Task 3: Create functions to run the checks

--- milestone_3.py
+++ milestone_3.py
@@ -3,15 +3,20 @@ import random
 word_list = ["Mango", "Pineapple", "Mangosteen", "Lychee", "Fig"]
 random_word = random.choice(word_list)
 
-while True:
-    letter_guess = input("Enter a single letter: ")
-    if len(letter_guess) == 1 and letter_guess.isalpha():
-        break
+def check_guess(guess):
+    lower_case_guess = guess.lower()
+    if lower_case_guess in random_word:
+        print(f"Good guess! {lower_case_guess} is in the word.")
     else:
-        print("Invalid letter. Please, enter a single alphabetical character.")
+        print(f"Sorry, {lower_case_guess} is not in the word. Try again.")
 
+def ask_for_input():
+    letter_guess = input("Enter a single letter: ")
+    while True:
+        if len(letter_guess) == 1 and letter_guess.isalpha():
+            break
+        else:
+            print("Invalid letter. Please, enter a single alphabetical character.")
+    check_guess(letter_guess)
 
-if letter_guess in random_word:
-    print(f"Good guess! {letter_guess} is in the word.")
-else:
-    print(f"Sorry, {letter_guess} is not in the word. Try again.")
+ask_for_input()
