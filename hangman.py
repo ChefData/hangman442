15b1b02a865cbb5825ad38e83dbe64f89916676a
ChefData <150172835+ChefData@users.noreply.github.com>
2023-11-24 15:05:43 +0000

    Update hangman.py
    Adds Docstrings describing the function, args & output of the various methods & class.
    Closes the connection after reading from the URL.
    Adds an “if __name__ == ‘__main__’:” code block to initialise the Hangman class and call the play_game method in.

--- hangman.py
+++ hangman.py
@@ -1,15 +1,22 @@
 import random
 from urllib.request import urlopen
 
-# word: The word to be guessed, picked randomly from the word_list. Remember to import the random module into your script
-# word_guessed: list - A list of the letters of the word, with _ for each letter not yet guessed. For example, if the word is 'apple', the word_guessed list would be ['_', '_', '_', '_', '_']. If the player guesses 'a', the list would be ['a', '_', '_', '_', '_']
-# num_letters: int - The number of UNIQUE letters in the word that have not been guessed yet
-# num_lives: int - The number of lives the player has at the start of the game.
-# word_list: list - A list of words
-# list_of_guesses: list - A list of the guesses that have already been tried. Set this to an empty list initially
 
 class Hangman:
+    '''
+    Hangman game class.
+    
+    Args:
+        word_list (list): List of words to choose from.
 
+    Attributes:
+        word_list (list): List of words to choose from.
+        num_lives (int): Number of lives the player starts with.
+        word (str): The word to guess.
+        word_guessed (list): List representing the current state of guessed letters in the word.
+        num_letters (int): Number of letters remaining to guess.
+        list_of_guesses (list): List of letters guessed by the player.
+    '''
     while True:
         num_lives = input("How many lives would you like? ")
         if not num_lives.isdigit():
@@ -26,7 +33,26 @@ class Hangman:
         self.num_letters = len(self.word)
         self.list_of_guesses = []
 
+    def display_final_message(self, is_winner):
+        """
+        Display the final message based on whether the player won or lost.
+
+        Args:
+            is_winner (bool): True if the player won, False otherwise.
+        """
+        if is_winner:
+            print(f"\nCongratulations. You won the game! The correct word was: {self.word}")
+        else:
+            print("\nYou lost!")
+            print(f"The correct word was: {self.word}")
+
     def check_guess(self, guess):
+        """
+        Check if the guessed letter is in the word and update the game state accordingly.
+
+        Args:
+            guess (str): The letter guessed by the player.
+        """
         if guess in self.word:
             print(f"Good guess! {guess} is in the word.")
             print(f"You still have {self.num_lives} lives left.")
@@ -40,6 +66,9 @@ class Hangman:
             print(f"You have {self.num_lives} lives left.")
 
     def ask_for_input(self):
+        """
+        Get a valid letter guess from the player and update the game state.
+        """
         while True:
             letter_guess = input("\nEnter a single letter: ").lower()
             if not letter_guess.isalpha() or len(letter_guess) != 1:
@@ -51,12 +80,18 @@ class Hangman:
                 self.list_of_guesses.append(letter_guess)
                 break 
 
+
 def play_game(word_list):
+    """
+    Play the Hangman game.
+
+    Args:
+        word_list (list): List of words to choose from.
+    """
     game = Hangman(word_list)
     while True:
         if game.num_lives == 0:
-            print("\nYou lost!")
-            print(f"The correct word was: {game.word}")
+            game.display_final_message(is_winner=False)
             break
         elif game.num_letters > 0:
             game.ask_for_input()
@@ -64,10 +99,19 @@ def play_game(word_list):
             print(f"Incorrect guesses: {', '.join(set(game.list_of_guesses) - set(game.word_guessed))}")
             print(f"Correct letters: {' '.join(game.word_guessed)}")
         else:
-            print(f"\nCongratulations. You won the game! The correct word was: {game.word}")
+            game.display_final_message(is_winner=True)
             break
 
-word_site = urlopen("https://www.mit.edu/~ecprice/wordlist.10000")
-word_list = word_site.read().decode().splitlines()
+def get_word_list():
+    """
+    Fetch and return a list of words from a URL.
 
-play_game(word_list)
+    Returns:
+        list: List of words.
+    """
+    with urlopen("https://www.mit.edu/~ecprice/wordlist.10000") as word_site:
+        return word_site.read().decode().splitlines()
+
+if __name__ == "__main__":
+    word_list = get_word_list()
+    play_game(word_list)
